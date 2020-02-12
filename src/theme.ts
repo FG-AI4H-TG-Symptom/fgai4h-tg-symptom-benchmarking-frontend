b03d59304e398a9107e8ef2474f6625e8b5ac6f6
clemens <clemens.schoell@ada.com>
2020-02-12 15:40:21 +0100

    Design: theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,15 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core'
 
-const themeCustomizations = createMuiTheme()
+const themeCustomizations = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1e88e5',
+    },
+    secondary: {
+      main: '#008f68',
+    },
+  },
+})
 
 themeCustomizations.typography.h1.fontSize = '2rem'
 themeCustomizations.typography.h3.fontSize = '1.5rem'
