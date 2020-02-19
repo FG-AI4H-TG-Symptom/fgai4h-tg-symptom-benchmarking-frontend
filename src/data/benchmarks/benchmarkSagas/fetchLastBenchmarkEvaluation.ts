FG-AI4H-TG-Symptom/fgai4h-tg-symptom-benchmarking-frontend

commit 61bbf6a628da4d006432bc6722736099f586c3a1

feat: create case set page + much improved overall error handling
- introduced application state to redux store (currently only handles fatal errors not caught anywhere else)
- merged redux store & logic for case set & case set list

src/data/benchmarks/benchmarkSagas/fetchLastBenchmarkEvaluation.ts
@@ -1,67 +1,80 @@
 import { call, put, select } from 'redux-saga/effects'
 
 import { RootState } from '../../rootReducer'
-import { fetchCaseSet as fetchCaseSetSaga } from '../../caseSets/caseSetsSagas'
+import { fetchCaseSet as fetchCaseSetSaga } from '../../caseSets/caseSetSagas'
 import { BenchmarkEvaluation } from '../benchmarkEvaluationDataType'
 import urlBuilder, { COMPONENTS } from '../../util/urlBuilder'
 import { lastBenchmarkEvaluationDataAction } from '../benchmarkActions'
 import { DataState } from '../../util/dataState/dataStateTypes'
+import httpResponseErrorMessage from '../../util/httpResponseErrorMessage'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function* fetchLastBenchmarkEvaluation() {
-  let state: RootState = yield select()
-  const benchmarkInfo = state.benchmark.currentBenchmarkingSession
-  if (!state.caseSets[benchmarkInfo.case_set_id]) {
-    yield call(fetchCaseSetSaga, benchmarkInfo.case_set_id)
-    state = yield select()
-  }
-
-  const caseSetLoadable = state.caseSets[benchmarkInfo.case_set_id]
-  if (caseSetLoadable.state !== DataState.READY) {
-    throw new Error('Case set not ready for benchmark evaluation')
-  }
-
-  const cases = caseSetLoadable.data
-
-  if (!benchmarkInfo) {
-    throw new Error('No benchmark to evaluate!')
-  }
-
-  if (!benchmarkInfo.finished) {
-    throw new Error('Trying to evaluate unfinished benchmark!')
-  }
-
-  const benchmarkEvaluation: BenchmarkEvaluation = {}
-
-  const aiNames = Object.keys(benchmarkInfo.ai_reports)
-  // eslint-disable-next-line no-restricted-syntax
-  for (const aiName of aiNames) {
-    const response = yield fetch(
-      urlBuilder(COMPONENTS.METRIC_CALCULATOR, 'calculate-metrics'),
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(
-          cases.map(({ caseData, valuesToPredict }, caseIndex) => ({
-            caseData,
-            valuesToPredict,
-            aiResult: benchmarkInfo.results_by_ai[aiName][caseIndex],
-          })),
-        ),
-      },
-    )
-
-    if (!response.ok) {
-      // todo: error handling
-      throw new Error('Errored while running benchmark on case set.')
+  try {
+    let state: RootState = yield select()
+    const benchmarkInfo = state.benchmark.currentBenchmarkingSession
+    if (!state.caseSets[benchmarkInfo.case_set_id]) {
+      yield call(fetchCaseSetSaga, benchmarkInfo.case_set_id)
+      state = yield select()
     }
 
-    const calculatedMetrics = yield response.json()
+    const caseSetLoadable = state.caseSets[benchmarkInfo.case_set_id]
+    if (caseSetLoadable.state !== DataState.READY) {
+      throw new Error('Case set not ready for benchmark evaluation')
+    }
 
-    benchmarkEvaluation[aiName] = calculatedMetrics
+    const cases = caseSetLoadable.data
+
+    if (!benchmarkInfo) {
+      throw new Error('No benchmark to evaluate!')
+    }
+
+    if (!benchmarkInfo.finished) {
+      throw new Error('Trying to evaluate unfinished benchmark!')
+    }
+
+    const benchmarkEvaluation: BenchmarkEvaluation = {}
+
+    const aiNames = Object.keys(benchmarkInfo.ai_reports)
+    // eslint-disable-next-line no-restricted-syntax
+    for (const aiName of aiNames) {
+      const response: Response = yield fetch(
+        urlBuilder(COMPONENTS.METRIC_CALCULATOR, 'calculate-metrics'),
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(
+            cases.map(({ caseData, valuesToPredict }, caseIndex) => ({
+              caseData,
+              valuesToPredict,
+              aiResult: benchmarkInfo.results_by_ai[aiName][caseIndex],
+            })),
+          ),
+        },
+      )
+
+      if (!response.ok) {
+        throw new Error(
+          httpResponseErrorMessage(
+            response,
+            `calculate metrics for AI '${aiName}'`,
+          ),
+        )
+      }
+
+      const calculatedMetrics = yield response.json()
+
+      benchmarkEvaluation[aiName] = calculatedMetrics
+    }
+
+    yield put(lastBenchmarkEvaluationDataAction.store(benchmarkEvaluation))
+  } catch (error) {
+    yield put(
+      lastBenchmarkEvaluationDataAction.errored(
+        `Errored while running benchmark on case set: ${error.message}`,
+      ),
+    )
   }
-
-  yield put(lastBenchmarkEvaluationDataAction.store(benchmarkEvaluation))
 }

src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux'
 import { Switch, Route, useLocation } from 'react-router-dom'
 import {
   AppBar,
@@ -15,8 +16,14 @@ import { routes } from './routes'
 import LinkWrapper from './components/Common/LinkWrapper'
 import NotFound from './components/Common/NotFound'
 import logo from './logo.svg'
+import { RootState } from './data/rootReducer'
+import Error from './components/Common/Error'
 
-const App: React.FC = () => {
+interface AppProps {
+  fatalError?: string
+}
+
+const App: React.FC<AppProps> = ({ fatalError }) => {
   const [menuOpen, setMenuOpen] = useState(false)
   const location = useLocation()
 
@@ -63,14 +70,23 @@ const App: React.FC = () => {
         </Styled.SideMenuList>
       </Drawer>
       <Styled.Main>
-        <Switch>
-          {routes.map(({ id, path, component, exact }) => (
-            <Route key={id} path={path} component={component} exact={exact} />
-          ))}
-          <Route component={NotFound} />
-        </Switch>
+        {fatalError ? (
+          <Error error={fatalError} />
+        ) : (
+          <Switch>
+            {routes.map(({ id, path, component, exact }) => (
+              <Route key={id} path={path} component={component} exact={exact} />
+            ))}
+            <Route component={NotFound} />
+          </Switch>
+        )}
       </Styled.Main>
     </>
   )
 }
-export default App
+
+const mapStateToProps = (state: RootState): AppProps => ({
+  fatalError: state.application.fatalError,
+})
+
+export default connect(mapStateToProps)(App)

src/components/BenchmarkCreator/BenchmarkCreatorComponent.tsx
@@ -18,7 +18,7 @@ import { ArrowForward as StartIcon } from '@material-ui/icons'
 import { useForm, ValidationResolver } from 'react-hook-form'
 
 import { AiImplementationInfo } from '../../data/aiImplementationList/aiImplementationDataType'
-import { CaseSetInfo } from '../../data/caseSetList/caseSetDataType'
+import { CaseSetInfo } from '../../data/caseSets/caseSetDataType'
 import { CreateBenchmarkManagerParameters } from '../../data/benchmarks/benchmarkActions'
 import ErrorIndicator from '../Common/ErrorIndicator'
 

src/components/BenchmarkCreator/BenchmarkCreatorContainer.tsx
@@ -12,8 +12,7 @@ import {
 import BenchmarkCreatorComponent from './BenchmarkCreatorComponent'
 import { AiImplementationListState } from '../../data/aiImplementationList/aiImplementationListReducers'
 import { RootState } from '../../data/rootReducer'
-import { CaseSetListState } from '../../data/caseSetList/caseSetListReducers'
-import { caseSetListDataActions } from '../../data/caseSetList/caseSetListActions'
+import { caseSetListDataActions } from '../../data/caseSets/caseSetActions'
 import {
   benchmarkManagerDataAction,
   CreateBenchmarkManagerParameters,
@@ -23,11 +22,12 @@ import { BenchmarkManager } from '../../data/benchmarks/benchmarkManagerDataType
 import { paths } from '../../routes'
 import Error from '../Common/Error'
 import BasicPageLayout from '../Common/BasicPageLayout'
+import { CaseSetInfo } from '../../data/caseSets/caseSetDataType'
 
 type AiImplementationManagerContainerDataProps = {
   aiImplementationList: AiImplementationListState
   benchmarkManager: Loadable<BenchmarkManager>
-  caseSetList: CaseSetListState
+  caseSetList: Loadable<CaseSetInfo[]>
 }
 type AiImplementationManagerContainerFunctionProps = {
   fetchAiImplementationList: (
@@ -111,7 +111,7 @@ const mapStateToProps: (
 ) => AiImplementationManagerContainerDataProps = state => ({
   aiImplementationList: state.aiImplementationList,
   benchmarkManager: state.benchmark.benchmarkManager,
-  caseSetList: state.caseSetList,
+  caseSetList: state.caseSets.overview,
 })
 const mapDispatchToProps: AiImplementationManagerContainerFunctionProps = {
   fetchAiImplementationList: aiImplementationListDataActions.load,

src/components/CaseSetCreator/CaseSetCreatorComponent.tsx
@@ -1,0 +1,99 @@
+import React from 'react'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  TextField,
+} from '@material-ui/core'
+import { ArrowForward as StartIcon } from '@material-ui/icons'
+import { useForm, ValidationResolver } from 'react-hook-form'
+
+import ErrorIndicator from '../Common/ErrorIndicator'
+import { CreateCaseSetParameters } from '../../data/caseSets/caseSetActions'
+
+interface FormData {
+  numberOfCases: number
+}
+type RawFormData = {
+  [fieldName in keyof FormData]?: string
+}
+
+const validationResolver: ValidationResolver = (rawValues: RawFormData) => {
+  const errors: { [fieldName in keyof FormData]?: string } = {}
+  const values: Partial<FormData> = {}
+
+  try {
+    values.numberOfCases = parseFloat(rawValues.numberOfCases)
+  } catch (error) {
+    errors.numberOfCases = 'Not a number'
+  }
+
+  if (values.numberOfCases <= 0) {
+    errors.numberOfCases = 'Enter a non-zero number of cases'
+  }
+  if (values.numberOfCases > 200) {
+    errors.numberOfCases = 'Please select a number of cases not exceeding 200'
+  }
+  return { values, errors }
+}
+
+interface CaseSetCreatorComponentProps {
+  onCreateCaseSet: (caseSetParameters: CreateCaseSetParameters) => void
+}
+
+const CaseSetCreatorComponent: React.FC<CaseSetCreatorComponentProps> = ({
+  onCreateCaseSet,
+}) => {
+  const { register, handleSubmit, errors } = useForm<FormData>({
+    validationResolver,
+    defaultValues: {
+      numberOfCases: 10,
+    },
+  })
+
+  const onSubmit = ({ numberOfCases }: FormData): void => {
+    onCreateCaseSet({
+      numberOfCases,
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardHeader
+              title='Basic parameters'
+              action={<ErrorIndicator error={errors.numberOfCases} />}
+            />
+            <CardContent>
+              <TextField
+                inputRef={register}
+                name='numberOfCases'
+                label='Number of cases'
+                type='number'
+                error={Boolean(errors.numberOfCases)}
+                helperText={errors.numberOfCases}
+              />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      <Box display='flex' justifyContent='flex-end' marginTop={4}>
+        <Button
+          variant='contained'
+          color='primary'
+          endIcon={<StartIcon />}
+          type='submit'
+        >
+          Create case set
+        </Button>
+      </Box>
+    </form>
+  )
+}
+
+export default CaseSetCreatorComponent

src/components/CaseSetCreator/CaseSetCreatorContainer.tsx
@@ -1,0 +1,67 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+import CaseSetCreatorComponent from './CaseSetCreatorComponent'
+import {
+  createCaseSetDataActions,
+  CreateCaseSetParameters,
+} from '../../data/caseSets/caseSetActions'
+import BasicPageLayout from '../Common/BasicPageLayout'
+import { RootState } from '../../data/rootReducer'
+import { DataState, Loadable } from '../../data/util/dataState/dataStateTypes'
+import DataStateManager from '../Common/DataStateManager'
+import { paths } from '../../routes'
+
+type CaseSetCreatorContainerDataProps = {
+  createdCaseSetId: Loadable<string>
+}
+type CaseSetCreatorContainerFunctionProps = {
+  createCaseSet: (caseSetParameters: CreateCaseSetParameters) => void
+  clearCreateCaseSet: () => void
+}
+type CasesSetCreatorProps = CaseSetCreatorContainerDataProps &
+  CaseSetCreatorContainerFunctionProps
+
+const CaseSetCreatorContainer: React.FC<CasesSetCreatorProps> = ({
+  createdCaseSetId,
+  createCaseSet,
+  clearCreateCaseSet,
+}) => {
+  const history = useHistory()
+  useEffect(() => {
+    if (createdCaseSetId.state === DataState.READY) {
+      history.push(paths.caseSetViewer(createdCaseSetId.data))
+    }
+
+    return (): void => {
+      setTimeout(clearCreateCaseSet, 0)
+    }
+  }, [createdCaseSetId, clearCreateCaseSet, history])
+
+  return (
+    <BasicPageLayout title='Generate a new case set'>
+      <DataStateManager
+        data={createdCaseSetId}
+        allowUninitialized
+        componentFunction={(): JSX.Element => (
+          <CaseSetCreatorComponent onCreateCaseSet={createCaseSet} />
+        )}
+      />
+    </BasicPageLayout>
+  )
+}
+
+const mapStateToProps: (
+  state: RootState,
+) => CaseSetCreatorContainerDataProps = state => ({
+  createdCaseSetId: state.caseSets.created,
+})
+const mapDispatchToProps: CaseSetCreatorContainerFunctionProps = {
+  createCaseSet: createCaseSetDataActions.load,
+  clearCreateCaseSet: createCaseSetDataActions.reset,
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CaseSetCreatorContainer)

src/components/CaseSetCreator/index.ts
@@ -1,0 +1,1 @@
+export { default } from './CaseSetCreatorContainer'

src/components/CaseSetManager/CaseSetManagerComponent.tsx
@@ -17,7 +17,7 @@ import {
 
 import LinkWrapper from '../Common/LinkWrapper'
 import { paths } from '../../routes'
-import { CaseSetInfo } from '../../data/caseSetList/caseSetDataType'
+import { CaseSetInfo } from '../../data/caseSets/caseSetDataType'
 
 import * as Styled from './CaseSetManagerComponent.style'
 import * as CommonStyled from '../Common/CommonStyles'

commit 953a7d5c513beaa308bae35bf8ea4c0e8080263a

fix: correct data access paths fetchLastBenchmarkEvaluation (regression introduced in 61bbf6a628da4d006432bc6722736099f586c3a1)

src/data/benchmarks/benchmarkSagas/fetchLastBenchmarkEvaluation.ts
@@ -13,12 +13,12 @@ export default function* fetchLastBenchmarkEvaluation() {
   try {
     let state: RootState = yield select()
     const benchmarkInfo = state.benchmark.currentBenchmarkingSession
-    if (!state.caseSets[benchmarkInfo.case_set_id]) {
+    if (!state.caseSets.entries[benchmarkInfo.case_set_id]) {
       yield call(fetchCaseSetSaga, benchmarkInfo.case_set_id)
       state = yield select()
     }
 
-    const caseSetLoadable = state.caseSets[benchmarkInfo.case_set_id]
+    const caseSetLoadable = state.caseSets.entries[benchmarkInfo.case_set_id]
     if (caseSetLoadable.state !== DataState.READY) {
       throw new Error('Case set not ready for benchmark evaluation')
     }