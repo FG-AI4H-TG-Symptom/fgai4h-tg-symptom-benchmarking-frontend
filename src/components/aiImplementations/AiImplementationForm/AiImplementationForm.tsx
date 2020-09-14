f652de901720e208bfae60071989f8ff67f6d6a7
Nils Strelow <1096841+nstrelow@users.noreply.github.com>
2020-09-14 16:25:09 +0200

    Do not use AutoTextField; Add validation (#29)
    * Do not use AutoTextField; Add validation
    
    * Add error message to Textfield

--- src/components/aiImplementations/AiImplementationForm/AiImplementationForm.tsx
+++ src/components/aiImplementations/AiImplementationForm/AiImplementationForm.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { Box, Button } from '@material-ui/core';
+import * as yup from 'yup';
 
-import AutoTextField from '../../forms/AutoTextField';
+import TextField from '@material-ui/core/TextField';
+import { yupResolver } from '@hookform/resolvers';
 
 interface AiImplementationFormProps {
   onSubmit: (aiImplementation) => void;
@@ -10,15 +12,20 @@ interface AiImplementationFormProps {
   baseUrl?: string;
   editing?: boolean;
 }
+
 const AiImplementationForm: React.FC<AiImplementationFormProps> = ({ onSubmit, editing = false, name, baseUrl }) => {
-  const methods = useForm<any>({
+  const aiImplementationSchema = yup.object().shape({
+    name: yup.string().label('Name').min(3).required(),
+    baseUrl: yup.string().label('Base URL').url().required(),
+  });
+
+  const { register, handleSubmit, errors, setValue } = useForm<any>({
     defaultValues: {
       name: name,
       baseUrl: baseUrl,
     },
-    // todo: validation based on OpenAPI schema
+    resolver: yupResolver(aiImplementationSchema),
   });
-  const { handleSubmit, setValue } = methods;
 
   useEffect(() => {
     setValue('name', name);
@@ -27,15 +34,30 @@ const AiImplementationForm: React.FC<AiImplementationFormProps> = ({ onSubmit, e
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormProvider {...methods}>
-        <AutoTextField name="name" type="text" label="Name" />
-        <AutoTextField name="baseUrl" type="text" label="Base URL" />
-        <Box display="flex" justifyContent="flex-end" marginTop={4}>
-          <Button color="primary" type="submit">
-            {editing ? 'Save' : 'Register'}
-          </Button>
-        </Box>
-      </FormProvider>
+      <TextField
+        inputRef={register}
+        name="name"
+        type="text"
+        label="Name"
+        fullWidth
+        error={Boolean(errors.name)}
+        helperText={errors.name?.message}
+      />
+      <Box m={4} />
+      <TextField
+        inputRef={register}
+        name="baseUrl"
+        type="text"
+        label="Base URL"
+        fullWidth
+        error={Boolean(errors.baseUrl)}
+        helperText={errors.baseUrl?.message}
+      />
+      <Box display="flex" justifyContent="flex-end" marginTop={4}>
+        <Button color="primary" type="submit">
+          {editing ? 'Save' : 'Register'}
+        </Button>
+      </Box>
     </form>
   );
 };
