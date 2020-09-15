1e27eb1a3986cac871a2af2ce128e4db8d5191a5
Nils Strelow <strelow.nils@gmail.com>
2020-09-15 16:49:39 +0200

    Add copy button for evaluation

--- src/components/common/ViewRaw.tsx
+++ src/components/common/ViewRaw.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Box, Button } from '@material-ui/core';
+import CopyIcon from '@material-ui/icons/FileCopy';
+import copy from 'copy-to-clipboard';
 import * as Styled from './ViewRaw.style';
 
-const ViewRaw: React.FC<{ data: any }> = ({ data }) => <Styled.RawView>{JSON.stringify(data, null, 2)}</Styled.RawView>;
+const ViewRaw: React.FC<{ data: any }> = ({ data }) => (
+  <Box flexGrow={1}>
+    <Styled.RawView>{JSON.stringify(data, null, 2)}</Styled.RawView>
+    <Button onClick={() => copy(JSON.stringify(data))} startIcon={<CopyIcon />}>
+      Copy
+    </Button>
+  </Box>
+);
 
 export default ViewRaw;
