99f65b2e68ce5c198b304be4549622ae8580f954
lebovka <ivan.lebovka.github@gmail.com>
2020-09-25 14:59:47 +0200

    bugfix

--- src/components/caseSets/SimpleCaseEditor/PresentingComplaintSection.tsx
+++ src/components/caseSets/SimpleCaseEditor/PresentingComplaintSection.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ClinicalFindingComponent from './ClinicalFindingComponent';
 import { FormBlock } from './FormElements';
 
 const PresentingComplaintSection: React.FC<any> = ({ case_, possibleClinicalFindings }) => {
-  const clinicalFinding = case_.data.caseData.presentingComplaints[0];
-
   const nameInObject = `caseData.presentingComplaints[0]`;
+
+  const [clinicalFinding, setClinicalFinding] = useState(null);
+
+  useEffect(() => {
+    setClinicalFinding(case_.data.caseData.presentingComplaints[0]);
+  }, []);
 
   return (
     <FormBlock name="presentingComplaints" color="#67c567" title="Presenting complaint">
-      <ClinicalFindingComponent
-        clinicalFinding={clinicalFinding}
-        nameInObject={nameInObject}
-        possibleClinicalFindings={possibleClinicalFindings}
-      />
+      {clinicalFinding && (
+        <ClinicalFindingComponent
+          clinicalFinding={clinicalFinding}
+          nameInObject={nameInObject}
+          possibleClinicalFindings={possibleClinicalFindings}
+          onClinicalFindingChange={(newId) => {
+            const newFinding = possibleClinicalFindings.find((cf) => cf.id === newId);
+            setClinicalFinding(newFinding);
+          }}
+        />
+      )}
     </FormBlock>
   );
 };
